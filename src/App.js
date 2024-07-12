05026881bc890c82337b1fdf5e7d638cd2f05e50
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-13 00:13:15 +0200

    make reducer currently with status: dataReceived, dataFailed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import Main from "./Main";
 import Header from "./Header";
+import { useEffect, useReducer } from "react";
+
+const initialState = { questions: [], status: ["loading"] };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
