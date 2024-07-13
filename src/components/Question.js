7c1301165bb2109256403a3c48de8fe9b8b31f11
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-13 15:08:22 +0200

    reorganize component files into a folder

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import Option from "./Option";
+
+function Question({ question }) {
+  console.log(question);
+
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Option question={question} />
+    </div>
+  );
+}
+
+export default Question;
