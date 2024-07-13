15ebe728f6654a9d3b9af1a5a2d8d2dd6f4c7b42
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-13 20:17:14 +0200

    Add NextButton component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Error from "./Error";
 import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
@@ -32,6 +33,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("Action unknown");
   }
@@ -62,11 +65,14 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />{" "}
+            <NextButton dispatch={dispatch} answer={answer}></NextButton>
+          </>
         )}
       </Main>
     </div>
