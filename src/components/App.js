cf29a73759ccc09636115c7b66e7b07d19ad9e01
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-13 20:29:24 +0200

    Create Progress to show the bar and question index, and points gained

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -41,12 +42,13 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((pre, cur) => pre + cur.points, 0);
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -66,11 +68,18 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
               answer={answer}
-            />{" "}
+            />
             <NextButton dispatch={dispatch} answer={answer}></NextButton>
           </>
         )}
