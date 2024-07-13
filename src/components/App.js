080e8dc46e5422adff92863166aaf512089ef054
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-13 15:42:21 +0200

    Add class to Disable hover effects on options once an answer is selected. Show different styles for correct and incorrect options after answering.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,12 @@ import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 
-const initialState = { questions: [], status: ["loading"], index: 0 };
+const initialState = {
+  questions: [],
+  status: ["loading"],
+  index: 0,
+  answer: null,
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -16,13 +21,15 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -45,7 +52,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
